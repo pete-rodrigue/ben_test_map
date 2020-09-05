cdc511eac655714f6ce58b47170ed167514310d2
pete rodrigue <edwardpierrerodrigue@gmail.com>
2020-09-05 00:45:04 -0500

    still more scroll attempts

--- script.js
+++ script.js
@@ -38,23 +38,76 @@ var utjson = {
 }
 
 
-function MyScrollTo (endPoint, duration) {
-  const distance = endPoint - window.pageYOffset,
-        rate = distance * 4 / duration, // px/4ms
-        interval = setInterval(scrollIncrement, 4); //4ms is minimum interval for browser
-  function scrollIncrement () {
-    if ((window.pageYOffset >= endPoint && rate >= 0) ||
-        (window.pageYOffset <= endPoint && rate <= 0))
+var EPPZScrollTo =
+{
+    /**
+     * Helpers.
+     */
+    documentVerticalScrollPosition: function()
     {
-      clearInterval(interval);
-    } else {
-      //keep in mind that scrollBy doesn't work with decimal pixels < 1 like 0.4px, so
-      //if duration is too big, function won't work. rate must end up being >= 1px
-      window.scrollBy(0, rate);
+        if (self.pageYOffset) return self.pageYOffset; // Firefox, Chrome, Opera, Safari.
+        if (document.documentElement && document.documentElement.scrollTop) return document.documentElement.scrollTop; // Internet Explorer 6 (standards mode).
+        if (document.body.scrollTop) return document.body.scrollTop; // Internet Explorer 6, 7 and 8.
+        return 0; // None of the above.
+    },
+    viewportHeight: function()
+    { return (document.compatMode === "CSS1Compat") ? document.documentElement.clientHeight : document.body.clientHeight; },
+    documentHeight: function()
+    { return (document.height !== undefined) ? document.height : document.body.offsetHeight; },
+    documentMaximumScrollPosition: function()
+    { return this.documentHeight() - this.viewportHeight(); },
+    elementVerticalClientPositionById: function(id)
+    {
+        var element = document.getElementById(id);
+        var rectangle = element.getBoundingClientRect();
+        return rectangle.top;
+    },
+    /**
+     * Animation tick.
+     */
+    scrollVerticalTickToPosition: function(currentPosition, targetPosition)
+    {
+        var filter = 0.2;
+        var fps = 60;
+        var difference = parseFloat(targetPosition) - parseFloat(currentPosition);
+        // Snap, then stop if arrived.
+        var arrived = (Math.abs(difference) <= 0.5);
+        if (arrived)
+        {
+            // Apply target.
+            scrollTo(0.0, targetPosition);
+            return;
+        }
+        // Filtered position.
+        currentPosition = (parseFloat(currentPosition) * (1.0 - filter)) + (parseFloat(targetPosition) * filter);
+        // Apply target.
+        scrollTo(0.0, Math.round(currentPosition));
+        // Schedule next tick.
+        setTimeout("EPPZScrollTo.scrollVerticalTickToPosition("+currentPosition+", "+targetPosition+")", (1000 / fps));
+    },
+    /**
+     * For public use.
+     *
+     * @param id The id of the element to scroll to.
+     * @param padding Top padding to apply above element.
+     */
+    scrollVerticalToElementById: function(id, padding)
+    {
+        var element = document.getElementById(id);
+        if (element == null)
+        {
+            console.warn('Cannot find element with id \''+id+'\'.');
+            return;
+        }
+        var targetPosition = this.documentVerticalScrollPosition() + this.elementVerticalClientPositionById(id) - padding;
+        var currentPosition = this.documentVerticalScrollPosition();
+        // Clamp.
+        var maximumScrollPosition = this.documentMaximumScrollPosition();
+        if (targetPosition > maximumScrollPosition) targetPosition = maximumScrollPosition;
+        // Start animation.
+        this.scrollVerticalTickToPosition(currentPosition, targetPosition);
     }
-  }
-}
-
+};
 
 // if the person hovers over a county, highlight it:
   let mouseOver = function(d) {
@@ -214,8 +267,7 @@ var our_counties = ["UTAH", "JUAB", "SANPETE", "SALT LAKE", "WASATCH"]
     
     // if you click on one of our counties, scroll to see the info below.
     if (our_counties.includes(d.properties['NAME'])) {
-      var target_element = document.getElementById("myPopup")
-      MyScrollTo(target_element.offsetTop, 240);
+      EPPZScrollTo.scrollVerticalToElementById('myPopup', 20);
     }
   }
 
